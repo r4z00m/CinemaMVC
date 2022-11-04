2cc0cbce7d5cd96ad11b820d17c407ff71fdde3c
r4z00m <w1shm4st3r@yandex.ru>
2022-11-04 16:47:05 +0300

    working

--- src/main/java/edu/school21/cinema/config/SpringConfiguration.java
+++ src/main/java/edu/school21/cinema/config/SpringConfiguration.java
@@ -1,16 +1,36 @@
 package edu.school21.cinema.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("edu.school21.cinema")
+@PropertySource("classpath:../webapp/WEB-INF/hibernate.properties")
+@EnableTransactionManagement
 public class SpringConfiguration {
+
+    private final Environment env;
+
+    @Autowired
+    public SpringConfiguration(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public FreeMarkerConfigurer freemarkerConfig() {
@@ -26,6 +46,46 @@ public class SpringConfiguration {
         resolver.setPrefix("");
         resolver.setSuffix(".ftl");
         return resolver;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("edu.school21.cinema.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(env.getRequiredProperty("hibernate.driver_class"));
+        dataSource.setUrl(env.getRequiredProperty("hibernate.connection.url"));
+        dataSource.setUsername(env.getRequiredProperty("hibernate.connection.username"));
+        dataSource.setPassword(env.getRequiredProperty("hibernate.connection.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect",
+                env.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql",
+                env.getRequiredProperty("hibernate.show_sql"));
+        return properties;
     }
 }
 
